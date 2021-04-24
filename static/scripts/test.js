b14966bcd13b007beb1bf0e4df0b3552e888e3a7
Eric05 <eboesch@hotmail.com>
2021-04-24 12:08:32 +0200

    separate view

---
+++ static/scripts/test.js
@@ -0,0 +1,19 @@
+        /*
+                form.onsubmit = async (e) => {
+                    e.preventDefault();
+
+                    fetch('/post', {
+                        method: 'POST',
+                        headers: {
+                            'Content-Type': 'application/json'
+                        },
+                        body: JSON.stringify({
+                            post: {
+                                name: document.forms["form"]["name"].value,
+                                text: document.forms["form"]["text"].value
+                            }
+                        })
+                    });
+                    addPost();
+                };
+        */
