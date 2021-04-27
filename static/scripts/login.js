42a79dbbeb689cbfa2c0276b1040a80d0952c802
Eric05 <eboesch@hotmail.com>
2021-04-27 20:47:30 +0200

    add css to index

--- static/scripts/login.js
+++ static/scripts/login.js
@@ -1,34 +1,34 @@
 function getName() {
     var user = localStorage.getItem("chatAppUser");
     name = (user == null) ? "" : user;
- 
-        document.forms["form"]["name"].value = name;
-    
+
+    document.forms["form"]["name"].value = name;
+
 }
 
-function login(){
+function login() {
     var user = document.forms["form"]["name"].value;
     localStorage.setItem("chatAppUser", user);
     window.location.replace("room1.html");
 }
-      
-      
-      /*
-                form.onsubmit = async (e) => {
-                    e.preventDefault();
 
-                    fetch('/post', {
-                        method: 'POST',
-                        headers: {
-                            'Content-Type': 'application/json'
-                        },
-                        body: JSON.stringify({
-                            post: {
-                                name: document.forms["form"]["name"].value,
-                                text: document.forms["form"]["text"].value
-                            }
-                        })
-                    });
-                    addPost();
-                };
-        */
+
+/*
+          form.onsubmit = async (e) => {
+              e.preventDefault();
+
+              fetch('/post', {
+                  method: 'POST',
+                  headers: {
+                      'Content-Type': 'application/json'
+                  },
+                  body: JSON.stringify({
+                      post: {
+                          name: document.forms["form"]["name"].value,
+                          text: document.forms["form"]["text"].value
+                      }
+                  })
+              });
+              addPost();
+          };
+  */
